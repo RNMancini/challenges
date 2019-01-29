057f1edfdf1e7155798d34f92120e4ea5ee271d2
Rebecca <rebeccamancini.student@careerdevs.com>
2019-01-29 01:23:25 +0000

    Draw 3x3 board

--- drawBoard.c
+++ drawBoard.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 
-int drawBoard()
-
 int main(void)
 {
+    int arr(1)(3)(3);
+    int x,o,j,y;
     
+    i = 0;
+    for(x=0;x<9;x++)
+        for(o=0;o<3;o++)
+            for(j=0;j<3;j++)
+                array[x][o] = i++;
+                
+                return(0)
+}
