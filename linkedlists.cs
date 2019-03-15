0e77ea73a836350443f705ce5c48044f4b0a69fa
Rebecca <rebeccamancini.student@careerdevs.com>
2019-03-15 00:08:37 +0000

    All tests passing

--- linkedlists.cs
+++ linkedlists.cs
@@ -1,25 +1,28 @@
-
 //Challenge continuation
+//
 ListNode<int> removeKFromList(ListNode<int> l, int k) {
     if (l == null) {
         return l;
+        
     }
+    Console.WriteLine(l.value);
+    
+        while(l.value == k) {
+            if (l.next == null) {
+                return l.next;
+            }
+            l = l.next;
+        }
     
     ListNode<int> trav = l;
     
     while(trav.next != null) {
-       Console.WriteLine(trav.value); 
-       trav = trav.next;
+        if (trav.next.value == k) {
+            trav.next = trav.next.next;
+            continue;
+            
+        }
+        trav = trav.next;
     }
-    Console.WriteLine(trav.value);    
-    return l;
-    }
-    //Remove function test 1
-    list.Remove(k);
-    Console.WriteLine(l);
-    foreach (var k in list) {
-        Console.WriteLine(l);
-    }
-    return l;
-    }
- }
+        return l;
+}
