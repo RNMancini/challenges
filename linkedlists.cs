e2c8e9358e5de54b09ce5f72b8cac491bf2f2cf9
Rebecca <rebeccamancini.student@careerdevs.com>
2019-03-15 00:48:22 +0000

    Simplification of linkedlists c#

--- linkedlists.cs
+++ linkedlists.cs
@@ -1,28 +1,13 @@
 //Challenge continuation
 //
 ListNode<int> removeKFromList(ListNode<int> l, int k) {
-    if (l == null) {
-        return l;
-        
+if (l == null) {
+    return l;
+}
+    l.next = removeKFromList(l.next, k);
+    if (l.value == k) {
+        return l.next;
     }
-    Console.WriteLine(l.value);
-    
-        while(l.value == k) {
-            if (l.next == null) {
-                return l.next;
-            }
-            l = l.next;
-        }
-    
-    ListNode<int> trav = l;
-    
-    while(trav.next != null) {
-        if (trav.next.value == k) {
-            trav.next = trav.next.next;
-            continue;
-            
-        }
-        trav = trav.next;
-    }
-        return l;
+return l;
+
 }
